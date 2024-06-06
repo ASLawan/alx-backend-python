0819f702f7f59d7fce8df2e43f9aaa9083deb0a9
ASLawan <nawalnitsua@gmail.com>
2024-06-06 11:05:04 +0100

    Write a type-annotated function that takes two float inputs and returns their sum with type float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Module implementing a function that returns the sum
+    of two float numbers.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of two float numbers"""
+    return a + b
