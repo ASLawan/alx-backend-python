e4d1fb5aa2d55b6f48802acf37923302bae6f661
ASLawan <nawalnitsua@gmail.com>
2024-06-06 11:47:14 +0100

    Implement a type-annotated function that takes a float and returns str format of the given float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Module implementing a function that returns string format
+    of given float input
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns string format of given float input"""
+    return str(n)
