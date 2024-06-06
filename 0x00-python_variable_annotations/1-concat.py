90b72effae97d3e796ba8415151d584a8ff2fa93
ASLawan <nawalnitsua@gmail.com>
2024-06-06 11:25:50 +0100

    Write a type annotated function that concatenates two strings and returns a string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Module implementing a function that concatenates two
+    substrings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two substrings into one"""
+    return "{}{}".format(str1, str2)
