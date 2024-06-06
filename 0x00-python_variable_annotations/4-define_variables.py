35ac168e47cb108be9b543685fac8ee5a02a36e2
ASLawan <nawalnitsua@gmail.com>
2024-06-06 11:58:16 +0100

    Define type-annotated variables for the given different data types

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Module defining annotated variables of different data
+    types
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
