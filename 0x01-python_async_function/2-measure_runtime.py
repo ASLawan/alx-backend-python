3d27d6b4b5f57e06182b26ba3176d4eb6adf5a3a
ASLawan <nawalnitsua@gmail.com>
2024-06-10 16:47:28 +0100

    Fix function type-annotation errors

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Returns average coroutine runtime"""
     start_time = time.perf_counter()
 
