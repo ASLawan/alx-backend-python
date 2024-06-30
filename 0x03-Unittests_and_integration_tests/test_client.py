e211ef6767c7ecaa29abc3cbc894b74bb928551e
ASLawan <nawalnitsua@gmail.com>
2024-06-30 14:21:27 +0100

    Implement TestGithubOrgClient class to run tests on GithubOrgClient class

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+    Module implementing test classes for GithubOrgClient
+
+"""
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from utils import get_json
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test suite for GithubOrgClient"""
+    @parameterized.expand([
+        ("google", {"login": "google", "id": 1, "repos_url":
+                             "https://api.github.com/orgs/google/repos"}),
+        ("abc", {"login": "abc", "id": 2, "repos_url":
+                \"https://api.github.com/orgs/abc/repos"}),
+    ])
+    @patch("client.get_json")
+    def test_org(self, org_name, expected_json, mock_get_json):
+        """Test GithubOrgClient"""
+        mock_get_json.return_value = expected_json
+
+        client = GithubOrgClient(org_name)
+        result = client.org
+
+        mock_get_json.assert_called_once_with
+        (GithubOrgClient.ORG_URL.format(org=org_name))
+        self.assertEqual(result, expected_json)
+
+
+if __name__ == "__main__":
+    unittest.main()
