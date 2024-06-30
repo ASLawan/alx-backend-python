be937f2acb2aeb3a18d0dd2074f57033f794ef5b
ASLawan <nawalnitsua@gmail.com>
2024-06-30 15:51:59 +0100

    Implement a method in TestGithubOrgClient class to test the public_repos method in GithubOrgClient class

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -42,6 +42,29 @@ class TestGithubOrgClient(unittest.TestCase):
 
             self.assertEqual(result, expected_payload["repos_url"])
 
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """Tests public_repos method"""
+        test_payload = [
+                {"name": "repo1", "license": {"key": "mit"}},
+                {"name": "repo2", "license": {"key": "apache-2.0"}},
+                {"name": "repo3", "license": {"key": "gpl-3.0"}}
+                ]
+        mock_get_json.return_value = test_payload
+        with patch("client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value =
+            "https://api.github.com/orgs/test/repos"
+            client = GithubOrgClient("test")
+            result = client.public_repos()
+
+            expected_repos = ["repo1", "repo2", "repo3"]
+            self.assertEqual(result, expected_repos)
+
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with
+            ("https://api.github.com/orgs/test/repos")
+
 
 if __name__ == "__main__":
     unittest.main()
